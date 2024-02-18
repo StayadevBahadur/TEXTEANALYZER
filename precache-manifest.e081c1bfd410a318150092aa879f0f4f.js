03af43642861c69c05a58b12ae8138ce4f643b14
StayadevBahadur <152260696+StayadevBahadur@users.noreply.github.com>
2024-02-18 16:20:19 +0530

    Updates

---
+++ precache-manifest.e081c1bfd410a318150092aa879f0f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf60d06b3ac4452ee539ace395580fd",
+    "url": "/TEXTEANALYZER/index.html"
+  },
+  {
+    "revision": "88f034b83df945609c31",
+    "url": "/TEXTEANALYZER/static/css/main.a8d77073.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/TEXTEANALYZER/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TEXTEANALYZER/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "88f034b83df945609c31",
+    "url": "/TEXTEANALYZER/static/js/main.9d8da24a.chunk.js"
+  },
+  {
+    "revision": "2aef52787ecf733eeb6b",
+    "url": "/TEXTEANALYZER/static/js/runtime~main.25cae638.js"
+  }
+]);
